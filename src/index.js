b9bc898a31cca4e5b931fc97d65feaaf21de7fdf
autumn crossan <the@autumn.zone>
2018-02-09 11:58:38 -0500

    refactored & flow-ified types

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-const stripChars = (str, chr) => str.replace(new RegExp(`\\${ chr }`, 'g'), '')
-const pipe = (init, ...funcs) => funcs.reduce((val, func) => func(val), init)
+// @flow
 
-const bold = str => str.match(/\*.+\*/) ? `<b>${ stripChars(str, '*') }</b>` : str
-const italic = str => str.match(/_.+_/) ? `<i>${ stripChars(str, '_') }</i>` : str
-const code = str => str.match(/`.+`/)  ? `<code>${ stripChars(str, '`') }</code>` : str
+const stripChars = (str: string, chr: string): string => 
+  str.replace(new RegExp(`\\${ chr }`, 'g'), '')
 
-export const convert = str => pipe(str, bold, italic, code)
+const pipe = <T>(init: T, ...funcs: Array<Function>): T => 
+  funcs.reduce((val: T, func: Function): T => func(val), init)
+
+const replace = (str: string, chr: string, tag: string): string => 
+  str.match(new RegExp(`\\${ chr }.+\\${ chr }`))
+    ? `<${ tag }>${ stripChars(str, chr) }</${ tag }>`
+    : str
+
+export const convert = (str: string): string => pipe(
+  str, 
+  str => replace(str, '*', 'b'),
+  str => replace(str, '_', 'i'), 
+  str => replace(str, '`', 'code')
+)
